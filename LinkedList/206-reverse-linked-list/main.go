722710ff3e9b3021a3a85927331f9a5494ccd02f
yanglyu2021 <thefloweroflife520@gmail.com>
2022-01-04 21:38:59 +1100

    add the readme file for question 206

---
+++ LinkedList/206-reverse-linked-list/main.go
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+    var prev *ListNode = nil
+    for head != nil {
+        //save the temporary next node the current head is pointing to
+        var next *ListNode = head.Next
+        //reverse the link direction
+        head.Next=prev
+        //move the prev pointer to the head node
+        prev=head
+        //move the head pointer to the next node
+        head = next
+    }
+    return prev
