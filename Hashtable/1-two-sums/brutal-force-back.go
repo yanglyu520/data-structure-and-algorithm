48e45cda6fe911a5b6f111f092783ca3ee83a834
Yang Lyu <42168739+yanglyu520@users.noreply.github.com>
2022-01-17 11:33:29 +1100

    Update brutal-force-back.go

--- Hashtable/1-two-sums/brutal-force-back.go
+++ Hashtable/1-two-sums/brutal-force-back.go
@@ -1,14 +1,11 @@
 func twoSum(nums []int, target int) []int {
-    result := []int{}
     for i,v := range nums{
-        fmt.Println(i)
         for j:=i+1;j<len(nums);j++ {
-            fmt.Println(i,j,target,v,target-v, nums[j])
             if nums[j] == target - v{
-                result = append(result, i,j)
+                return []int{i,j}
             }
         }
     }
-    return result
+    return nil
     
 }
